cedfc5ea850f31ac8c3e373d12af1636689c17dc
Hilliam T <hilliamtung@gmail.com>
2024-06-18 01:23:08 +0100

    added in fast / best mode in multiquoter

--- src/models/MultiQuoter.ts
+++ src/models/MultiQuoter.ts
@@ -1,10 +1,14 @@
-import { InputAmountRequest, OutputAmountRequest, PriceRequest, Quoter } from "@aori-io/sdk";
+import { InputAmountRequest, OutputAmountRequest, PriceRequest, Quote, Quoter } from "@aori-io/sdk";
+
+export type Mode = "fast" | "best";
 
 export class MultiQuoter implements Quoter {
     quoters: Quoter[];
+    mode: Mode;
 
-    constructor(quoters: Quoter[]) {
+    constructor(quoters: Quoter[], mode: Mode = "fast") {
         this.quoters = quoters;
+        this.mode = mode;
     }
 
     static build(quoters: Quoter[]) {
@@ -16,7 +20,16 @@ export class MultiQuoter implements Quoter {
     }
 
     async getOutputAmountQuote({ inputToken, outputToken, inputAmount, fromAddress, chainId }: OutputAmountRequest) {
-        const responses = await Promise.allSettled(this.quoters.map(quoter => quoter.getOutputAmountQuote({ inputToken, outputToken, inputAmount, fromAddress, chainId }))); 
+        const allQuotes = this.quoters.map(quoter => quoter.getOutputAmountQuote({ inputToken, outputToken, inputAmount, fromAddress, chainId }));
+        if (this.mode == "fast") {
+            try {
+                return await Promise.any(allQuotes);
+            } catch (error) {
+                return { price: 0, gas: 0n, outputAmount: 0n, to: "", value: 0, data: "" };
+            }
+        }
+
+        const responses = await Promise.allSettled(allQuotes); 
 
         // Out of those that were successful, return the one with the highest output amount
         const bestQuote = responses.reduce((best, response) => {
@@ -30,7 +43,16 @@ export class MultiQuoter implements Quoter {
     }
 
     async getInputAmountQuote({ inputToken, outputToken, outputAmount, fromAddress, chainId }: InputAmountRequest) {
-        const responses = await Promise.allSettled(this.quoters.map(quoter => quoter.getInputAmountQuote({ inputToken, outputToken, outputAmount, fromAddress, chainId }))); 
+        const allQuotes = this.quoters.map(quoter => quoter.getInputAmountQuote({ inputToken, outputToken, outputAmount, fromAddress, chainId }));
+        if (this.mode == "fast") {
+            try {
+                return await Promise.any(allQuotes);
+            } catch (error) {
+                return { price: 0, gas: 0n, outputAmount: 0n, to: "", value: 0, data: "" };
+            }
+        }
+
+        const responses = await Promise.allSettled(allQuotes); 
 
         // Out of those that were successful, return the one with the lowest input (output) amount
         const bestQuote = responses.reduce((best, response) => {
